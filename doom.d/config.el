43768f9dfca60062aab54cfb4af1507d46b86900
Bror Winther <browin@rm.dk>
2020-09-25 02:22:09 +0200

    Removes waka api key

--- doom.d/config.el
+++ doom.d/config.el
@@ -164,7 +164,6 @@
 (use-package wakatime-mode
   :hook doom-first-buffer
   :config
-  (setq wakatime-api-key "03771d24-8c20-4b1a-a4a1-8ab46b33f333")
   (cond ((eq system-type 'darwin)
 	 (setq wakatime-cli-path "/usr/local/bin/wakatime"))
 	(setq wakatime-python-bin "/usr/local/bin/python3")
