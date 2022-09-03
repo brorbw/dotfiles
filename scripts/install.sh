43c07cb219ac2a2d0290aeacd5b26ff9f981ffaa
Bror Winther <git@nobad.coffee>
2022-09-03 02:11:31 +0200

    Install stuff for iTerm and zsh

--- scripts/install.sh
+++ scripts/install.sh
@@ -112,10 +112,11 @@ duti ~/.config/duti.conf
 brew install rbenv
 brew install ruby-build
 
-#sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-#ZSH_CUSTOM=~/.oh-my-zsh
-#git clone https://github.com/denysdovhan/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt" --depth=1
-#ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+ZSH_CUSTOM=~/.oh-my-zsh
+git clone https://github.com/denysdovhan/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt" --depth=1
+ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
+cd ~/.oh-my-zsh/custom/plugins && git clone https://github.com/wbingli/zsh-wakatime.git
 
 # custom stuff
 brew install koekeishiya/formulae/yabai
