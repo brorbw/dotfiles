6531e44c2ab3baf85ba012544fc3a4e700cd1005
Bror Winther <git@nobad.coffee>
2022-09-20 03:04:36 +0200

    Add, add repos script

--- add-repos-as-projects.sh
+++ add-repos-as-projects.sh
@@ -4,13 +4,13 @@ rm ~/.emacs.d/.local/cache/projectile.projects
 emacsclient --eval "(projectile-add-known-project \"~/.config\")" >/dev/null
 
 find ~/clearhaus -type d -name ".git" -maxdepth 4 |
-	xargs dirname |
-	xargs -L 1 -I{} emacsclient --eval "(projectile-add-known-project \"{}\")" >/dev/null
+  xargs dirname |
+  xargs -L 1 -I{} emacsclient --eval "(projectile-add-known-project \"{}\")" >/dev/null
 
 find ~/Projects -type d -name ".git" -maxdepth 4 |
-	xargs dirname |
-	xargs -L 1 -I{} emacsclient --eval "(projectile-add-known-project \"{}\")" >/dev/null
+  xargs dirname |
+  xargs -L 1 -I{} emacsclient --eval "(projectile-add-known-project \"{}\")" >/dev/null
 
-find ~/dias -type d -name ".git" -maxdepth 4 |
-	xargs dirname |
-	xargs -L 1 -I{} emacsclient --eval "(projectile-add-known-project \"{}\")" >/dev/null
+# find ~/dias -type d -name ".git" -maxdepth 4 |
+# 	xargs dirname |
+# 	xargs -L 1 -I{} emacsclient --eval "(projectile-add-known-project \"{}\")" >/dev/null
