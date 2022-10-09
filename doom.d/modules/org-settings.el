3c4dd77310509795bbbfd1a2af5ad8972ea2964b
Bror Winther <git@nobad.coffee>
2022-10-10 01:31:38 +0200

    Add options for future stuf

--- doom.d/modules/org-settings.el
+++ doom.d/modules/org-settings.el
@@ -2,3 +2,7 @@
 
 ;; setting org directory - it should be backuped somehow (for now iCloud)
 (setq org-directory "~/Library/Mobile Documents/iCloud~com~appsonthemove~beorg/Documents/org")
+
+;; Something to add in the future maybe?
+;; (add-to-list 'org-modules 'org-mac-iCal)
+;; (setq org-agenda-include-diary t)
