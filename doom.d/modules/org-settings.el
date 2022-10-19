0e0a1613393ab68e22a95c4be6d66ee851df53ec
Bror Winther <git@nobad.coffee>
2022-10-19 14:39:13 +0200

    Org settings

--- doom.d/modules/org-settings.el
+++ doom.d/modules/org-settings.el
@@ -1,7 +1,11 @@
 ;;; $DOOMDIR/modules/org.el -*- lexical-binding: t; -*-
 
+;; (require 'org-protocol)
+
 ;; setting org directory - it should be backuped somehow (for now iCloud)
 (setq org-directory "~/Library/Mobile Documents/iCloud~com~appsonthemove~beorg/Documents/org")
+;; (org-projectile-per-project)
+;; (setq org-agenda-files (append org-agenda-files (org-projectile-todo-files)))
 
 ;; Something to add in the future maybe?
 ;; (add-to-list 'org-modules 'org-mac-iCal)
