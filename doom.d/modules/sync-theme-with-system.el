75a1741f030efb658167895760d15019d30922ca
Bror Bisgaard Winther <bbw@clearhaus.com>
2022-10-19 12:57:24 +0000

    Fix theme sync to take tty into account

--- doom.d/modules/sync-theme-with-system.el
+++ doom.d/modules/sync-theme-with-system.el
@@ -1,25 +1,34 @@
 ;;; $DOOMDIR/modules/sync-theme-with-system.el -*- lexical-binding: t; -*-
-
-(defcustom me/dark-theme-name
+(defcustom me/dark-theme
   'doom-moonlight
   "Dark theme value")
-(defcustom me/light-theme-name
+(defcustom me/light-theme
   'doom-nord-light
   "Light theme value")
 
-(defun me/apply-theme (appearance)
-  "Load theme taking the systems current appearance into consideration."
-  (message "theme changed" appearance)
-  (mapc #'disable-theme custom-enabled-themes)
-  (pcase appearance
-    ('light (load-theme me/light-theme-name t))
-    ('dark (load-theme me/dark-theme-name t))))
+(defcustom me/tty-theme
+  'doom-one
+  "Dark theme for tty")
 
-(add-hook 'ns-system-appearance-change-functions #'me/apply-theme)
+;; set theme for tty sessions
+(unless (display-graphic-p)
+  (setq doom-theme me/tty-theme))
 
-(defun me/load-theme-when-booted ()
-(pcase ns-system-appearance
-('light (load-theme me/light-theme-name t))
-('dark (load-theme me/dark-theme-name t))))
+;; set theme for macos
+(when (eq system-type 'darwin)
+  (defun me/apply-theme (appearance)
+    "Load theme taking the systems current appearance into consideration."
+    (message "theme changed" appearance)
+    (mapc #'disable-theme custom-enabled-themes)
+    (pcase appearance
+      ('light (load-theme me/light-theme t))
+      ('dark (load-theme me/dark-theme t))))
 
-(me/load-theme-when-booted)
+  (add-hook 'ns-system-appearance-change-functions #'me/apply-theme)
+
+  (defun me/load-theme-when-booted ()
+    (pcase ns-system-appearance
+      ('light (load-theme me/light-theme t))
+      ('dark (load-theme me/dark-theme t))))
+
+  (me/load-theme-when-booted))
