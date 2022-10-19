05004c099d5fe4defefa3a49912321aa7dfdc3dd
Bror Winther <git@nobad.coffee>
2022-10-19 14:40:11 +0200

    Default project switch is magit

--- doom.d/modules/projectile-settings.el
+++ doom.d/modules/projectile-settings.el
@@ -4,3 +4,4 @@
 
 ;; Only use this if we want the default action to be dired could be somethin
 ;; (setq projectile-switch-project-action #'projectile-dired)
+(setq projectile-switch-project-action #'magit-status)
