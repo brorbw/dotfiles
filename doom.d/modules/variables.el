f6a878d9ee7902516558d670b622d6ed047b9089
Bror Winther <git@nobad.coffee>
2023-03-13 01:09:13 +0100

    Set variables for c++ in unreal

--- doom.d/modules/variables.el
+++ doom.d/modules/variables.el
@@ -56,17 +56,18 @@
 ;;                                 "--header-insertion-decorators=0"))
 ;; (after! lsp-clangd (set-lsp-priority! 'clangd 2))
 
-(setq lsp-clients-clangd-args '("-j=3"
-				"--all-scopes-completion"
-                                "--background-index"
-                                "--clang-tidy"
-                                "--completion-style=detailed"
-                                "--header-insertion=iwyu"
-                                "--header-insertion-decorators=0"))
-(after! lsp-clangd (set-lsp-priority! 'clangd 2))
-;; (after! ccls
-;;   (setq ccls-initialization-options '(:index (:comments 2) :completion (:detailedLabel t)))
-;;   (set-lsp-priority! 'ccls 2)) ; optional as ccls is the
+;; (setq lsp-clients-clangd-args '("-j=3"
+;; 				"--all-scopes-completion"
+;;                                 "--background-index"
+;;                                 "--clang-tidy"
+;;                                 "--completion-style=detailed"
+;;                                 "--header-insertion=iwyu"
+;;                                 "--header-insertion-decorators=0"))
+;; (after! lsp-clangd (set-lsp-priority! 'clangd 2))
+
+(after! ccls
+  (setq ccls-initialization-options '(:index (:comments 2) :completion (:detailedLabel t)))
+  (set-lsp-priority! 'ccls 2))
 
 (setq! +ligatures-in-modes '(elisp-mode))
 
