aab936b8fde0fa82a683b1da7c71159b626fc45a
Bror Winther <git@nobad.coffee>
2023-03-31 16:53:20 +0200

    Change default action for change project to default

--- doom.d/modules/projectile-settings.el
+++ doom.d/modules/projectile-settings.el
@@ -3,5 +3,4 @@
 (setq projectile-project-search-path '("~/Projects" "~/clearhaus"))
 
 ;; Only use this if we want the default action to be dired could be somethin
-;; (setq projectile-switch-project-action #'projectile-dired)
-(setq projectile-switch-project-action #'magit-status)
+;; (setq projectile-switch-project-action #'magit-status)
