020b997415d488077a85375211521aac5e87cf24
Bror Winther <git@nobad.coffee>
2023-08-15 02:41:05 +0200

    Fix scripts that use absolute path

--- scripts/select_space_on_current_display.sh
+++ scripts/select_space_on_current_display.sh
@@ -9,20 +9,20 @@
 readonly SPACE_INDEX=$1
 # echo "SPACE_INDEX=$SPACE_INDEX" | ts | tee -a $LOG_FILE
 
-readonly ACTIVE_DISPLAY_ID=$(/usr/local/bin/yabai -m query --spaces |
-	/usr/local/bin/jq '.[] | select(."has-focus" == true).display')
+readonly ACTIVE_DISPLAY_ID=$(yabai -m query --spaces |
+	jq '.[] | select(."has-focus" == true).display')
 # echo "ACTIVE_DISPLAY_ID=$ACTIVE_DISPLAY_ID" | ts | tee -a $LOG_FILE
 
-readonly SPACES_OF_ACTIVE_DISPLAY=$(/usr/local/bin/yabai -m query --displays |
-	/usr/local/bin/jq ".[] | select(.index == $ACTIVE_DISPLAY_ID).spaces")
+readonly SPACES_OF_ACTIVE_DISPLAY=$(yabai -m query --displays |
+	jq ".[] | select(.index == $ACTIVE_DISPLAY_ID).spaces")
 # echo "SPACES_OF_ACTIVE_DISPLAY=$SPACES_OF_ACTIVE_DISPLAY" | ts | tee -a $LOG_FILE
 
 readonly TARGET_SPACE=$(echo $SPACES_OF_ACTIVE_DISPLAY |
-	/usr/local/bin/jq -r ".[$SPACE_INDEX]")
+	jq -r ".[$SPACE_INDEX]")
 # echo "TARGET_SPACE=$TARGET_SPACE" | ts | tee -a $LOG_FILE
 
 # We do the below twice, because yabai is broken and in some scenarios doesn't focus the window
 # on the space it just switched to. Instead, it'll switch to the right space, but then focus the
 # window on the main monitor. This fixes it.
-/usr/local/bin/yabai -m space --focus $TARGET_SPACE
-/usr/local/bin/yabai -m space --focus $TARGET_SPACE
+yabai -m space --focus $TARGET_SPACE
+yabai -m space --focus $TARGET_SPACE
