9e1ab2a8fcd5ec28cedfa0481ad545c38ebd068c
Bror Winther <bbw@clearhaus.com>
2023-09-27 09:33:24 +0200

    Fix install script

--- scripts/install.sh
+++ scripts/install.sh
@@ -127,9 +127,11 @@ curl -fsSL https://raw.githubusercontent.com/spicetify/spicetify-cli/master/inst
 git clone https://github.com/spicetify/spicetify-themes.git /tmp
 cp /tmp/spicetify-themes/* ~/.config/spicetify/Themes/
 
+git config --global commit.gpgsign true
 
 echo "Remeber to set waka api key or emacs will bug you"
 echo "wakatime --config-write api_key=<API_KEY>"
+echo "wakatime --config-write api_url=<WAKA_API_URL>"
 
 if $AMD_HACKINTOSH; then
 	files_list=(MMXCore FastCore TextModel libiomp5.dylib)
