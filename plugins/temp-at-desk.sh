616b2043ef8e89e5d0b87f196b14aecee11b924d
Bror Winther <git@nobad.coffee>
2023-11-04 20:47:05 +0100

    Add temp at desk script

---
+++ plugins/temp-at-desk.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+res_from_ha=$(curl -s -X GET https://ha.nobad.coffee/api/states/sensor.livingroom_sonoff_snzb_02_temperature -H "Authorization: Bearer $(cat $HOME/.ha-token)" | jq -r '.state')
+sketchybar --set $NAME label="$res_from_ha"
